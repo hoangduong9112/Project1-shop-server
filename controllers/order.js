31cea43ca2d8583dc8ba22e8920348cfd74dc721
duonghoang9112 <duonghoang9112@gmail.com>
2023-01-02 00:15:32 +0700

    fix: use promise to query

--- controllers/order.js
+++ controllers/order.js
@@ -2,46 +2,45 @@ import { v4 as uuidv4 } from 'uuid';
 import { clientPG } from '../database.js';
 
 export const createOrder = async (req, res) => {
-  try {
-    const orderId = uuidv4().toString();
-    const orderResult = await clientPG.query(
+  const orderId = uuidv4().toString();
+  const orderResult = await clientPG
+    .query(
       `INSERT INTO orders (order_id, user_name, phone, address) VALUES ($1, $2, $3, $4)`,
       [orderId, req.body.user_name, req.body.phone, req.body.address],
+    )
+    .then(() => {
+      req.body.packages.forEach(async (pack) => {
+        clientPG.query(
+          `INSERT INTO packages (product_id, order_id, quantity) VALUES ($1, $2, $3)`,
+          [pack.product_id, orderId, pack.quantity],
+        );
+      });
+    })
+    .then(() => res.status(200).json({ message: 'Create Successful' }))
+    .catch((e) =>
+      res.status(400).json({
+        message: 'Create Fail',
+        error: e,
+      }),
     );
-    const packagesResult = req.body.packages.map(async (pack) => {
-      const packResult = await clientPG.query(
-        `INSERT INTO packages (product_id, order_id, quantity) VALUES ($1, $2, $3)`,
-        [pack.product_id, orderId, pack.quantity],
-      );
-      return packResult;
-    });
-    res.status(200).json({
-      message: 'Create Order Successful',
-      data: [],
-    });
-  } catch (err) {
-    res.status(400).json({
-      message: 'Create Order Fail',
-      error: err,
-    });
-  }
 };
 
 export const getOrderList = async (req, res) => {
-  try {
-    const result = await clientPG.query(
+  clientPG
+    .query(
       `select * from orders o
       left join packages pack ON pack.order_id = o.order_id
       left join products prod ON pack.product_id = prod.product_id`,
+    )
+    .then((result) =>
+      res
+        .status(200)
+        .json({ message: 'Get Order Successful', data: result.rows }),
+    )
+    .catch((e) =>
+      res.status(400).json({
+        message: 'Get Order Fail',
+        error: e,
+      }),
     );
-    res.status(200).json({
-      message: 'Get Order Successful',
-      data: result.rows,
-    });
-  } catch (err) {
-    res.status(400).json({
-      message: 'Get Order Fail',
-      error: err,
-    });
-  }
 };
--- controllers/product.js
+++ controllers/product.js
@@ -1,76 +1,78 @@
 import { clientPG } from '../database.js';
 
 export const getProducts = async (req, res) => {
-  try {
-    const result = await clientPG.query(`SELECT * FROM products`);
-    res.status(200).json({ data: result.rows });
-  } catch (err) {
-    res.status(400).json({
-      message: 'Get Fail',
-      error: err,
-    });
-  }
+  clientPG
+    .query(`SELECT * FROM products`)
+    .then((result) =>
+      res.status(200).json({ message: 'Get Successful', data: result.rows }),
+    )
+    .catch((e) =>
+      res.status(400).json({
+        message: 'Get Fail',
+        error: e,
+      }),
+    );
 };
 
 export const getProductById = async (req, res) => {
-  try {
-    const result = await clientPG.query(
-      `SELECT * FROM products where product_id = ${req.params.id}`,
+  clientPG
+    .query(`SELECT * FROM products where product_id = ${req.params.id}`)
+    .then((result) =>
+      res.status(200).json({ message: 'Get Successful', data: result.rows[0] }),
+    )
+    .catch((e) =>
+      res.status(400).json({
+        message: 'Get Fail',
+        error: e,
+      }),
     );
-    res.status(200).json({ data: result.rows[0] });
-  } catch (err) {
-    res.status(400).json({
-      message: 'Get Fail',
-      error: err,
-    });
-  }
 };
 
 export const createProduct = async (req, res) => {
-  try {
-    const result = await clientPG.query(
+  clientPG
+    .query(
       `INSERT INTO products (name, description, price) VALUES ($1, $2, $3)`,
       [req.body.name, req.body.description, req.body.price],
+    )
+    .then(() => res.status(200).json({ message: 'Create Successful' }))
+    .catch((e) =>
+      res.status(400).json({
+        message: 'Create Fail',
+        error: e,
+      }),
     );
-    res.status(200).json({ message: 'Create Successful', data: result.rows });
-  } catch (err) {
-    res.status(400).json({
-      message: 'Create Fail',
-      error: err,
-    });
-  }
 };
 
 export const updateProduct = async (req, res) => {
-  try {
-    const result = await clientPG.query(
+  clientPG
+    .query(
       `UPDATE products
       SET name = $1,
           description = $2,
           price = $3
       WHERE product_id = ${req.body.product_id};`,
       [req.body.name, req.body.description, req.body.price],
+    )
+    .then(() => res.status(200).json({ message: 'Update Successful' }))
+    .catch((e) =>
+      res.status(400).json({
+        message: 'Update Fail',
+        error: e,
+      }),
     );
-    res.status(200).json({ message: 'Update Successful', data: result.rows });
-  } catch (err) {
-    res.status(400).json({
-      message: 'Update Fail',
-      error: err,
-    });
-  }
 };
 
 export const deleteProduct = async (req, res) => {
-  try {
-    const result = await clientPG.query(
+  clientPG
+    .query(
       `DELETE FROM products
       WHERE product_id = ${req.body.product_id};`,
+    )
+    .then(() => res.status(200).json({ message: 'Delete Successful' }))
+    .catch((e) =>
+      res.status(400).json({
+        message: 'Delete Fail',
+        error: e,
+      }),
     );
-    res.status(200).json({ message: 'Delete Successful', data: result.rows });
-  } catch (err) {
-    res.status(400).json({
-      message: 'Delete Fail',
-      error: err,
-    });
-  }
 };
