e531292eebc1b6c2dc95a4437869ae7b05e0d170
duonghoang9112 <duonghoang9112@gmail.com>
2023-01-03 23:12:54 +0700

    fix: modify result from db

--- controllers/order.js
+++ controllers/order.js
@@ -1,9 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
+import findIndex from 'lodash/findIndex.js';
 import { clientPG } from '../database.js';
 
 export const createOrder = async (req, res) => {
   const orderId = uuidv4().toString();
-  const orderResult = await clientPG
+  clientPG
     .query(
       `INSERT INTO orders (order_id, user_name, phone, address) VALUES ($1, $2, $3, $4)`,
       [orderId, req.body.user_name, req.body.phone, req.body.address],
@@ -32,11 +33,43 @@ export const getOrderList = async (req, res) => {
       left join packages pack ON pack.order_id = o.order_id
       left join products prod ON pack.product_id = prod.product_id`,
     )
-    .then((result) =>
+    .then((result) => {
+      const orderList = [];
+      result.rows.forEach((row) => {
+        const existIndex = findIndex(orderList, (order) => {
+          return order.order_id === row.order_id;
+        });
+        if (existIndex > -1) {
+          orderList[existIndex].products.push({
+            product_id: row.product_id,
+            quantity: row.quantity,
+            name: row.name,
+            description: row.description,
+            price: row.price,
+          });
+        } else {
+          orderList.push({
+            order_id: row.order_id,
+            user_name: row.user_name,
+            phone: row.phone,
+            address: row.address,
+            package_id: row.package_id,
+            products: [
+              {
+                product_id: row.product_id,
+                quantity: row.quantity,
+                name: row.name,
+                description: row.description,
+                price: row.price,
+              },
+            ],
+          });
+        }
+      });
       res
         .status(200)
-        .json({ message: 'Get Order Successful', data: result.rows }),
-    )
+        .json({ message: 'Get Order Successful', data: orderList });
+    })
     .catch((e) =>
       res.status(400).json({
         message: 'Get Order Fail',
